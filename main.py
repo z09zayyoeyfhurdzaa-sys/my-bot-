3a842a3af13478c6d392c0ddf2fa03aefae392fa
z09zayyoeyfhurdzaa-sys <z09zayyoeyfhurdzaa@gmail.com>
2026-04-13 08:41:54 +0300

    Change bot token in main.py
    Updated the bot token for authentication.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telebot import types
 import json, os
 
 # --- الإعدادات الصحيحة من صورك ---
-TOKEN = "8372753026:AAG7SJLu_FkLrz-MzPJXNNE4D_5hyemyLlU"
+TOKEN = "8372753026:AAHQ3k9hEkJqVJK2kGATb5wTZHgHjUim9HM"
 ADMIN_ID = 7557584016
 DATA_FILE = "bot_database.json"
 
